617974519547e358676bfcf8b6078c9b57cefd55
MykleR <michael.rouves@gmail.com>
2025-01-29 00:02:54 +0100

    update

--- headers/philo.h
+++ headers/philo.h
@@ -6,7 +6,7 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 19:44:28 by mrouves           #+#    #+#             */
-/*   Updated: 2025/01/28 20:04:11 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/01/28 20:23:16 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,23 @@
 # include <pthread.h>
 # include <stdbool.h>
 # include <stdio.h>
+
+typedef struct s_philo
+{
+	uint16_t	id;
+	uint32_t	count_die;
+	uint32_t	count_eat;
+	uint32_t	count_sleep;
+}	t_philo;
+
+typedef struct s_manager
+{
+	uint32_t	delay_die;
+	uint32_t	delay_eat;
+	uint32_t	delay_sleep;
+	uint16_t	nb_eats;
+	uint16_t	nb_philo;
+}	t_manager;
 
 bool	safe_atou(const char *str, uint32_t *out);
 
--- sources/utils.c
+++ sources/utils.c
@@ -6,13 +6,30 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 20:02:02 by mrouves           #+#    #+#             */
-/*   Updated: 2025/01/28 20:02:41 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/01/28 20:16:17 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <philo.h>
 
-bool	safe_atou(const char *str, uint32_t *out)
+bool	safe_atou(const char *s, uint32_t *out)
 {
+	uint64_t	res;
 
+	if (__builtin_expect(!s || !out, 0))
+		return (false);
+	res = 0;
+	while (*s == ' ' || *s == '\t' || *s == '\r')
+		s++;
+	s += (*s == '+');
+	if (!(*s))
+		return (false);
+	while (*s)
+	{
+		res = (res << 1) + (res << 3) + (*s) - '0';
+		if (*s < '0' || *s++ > '9' || res > UINT32_MAX)
+			return (false);
+	}
+	*out = res;
+	return (true);
 }
